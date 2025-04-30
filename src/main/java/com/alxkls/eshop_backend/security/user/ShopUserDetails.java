b301a1a70ea2f385459768a65e06bccc6153c612
alxkls <oleksikoleso@gmail.com>
2025-04-30 21:04:43 +0100

    upd: introduce spring security

--- src/main/java/com/alxkls/eshop_backend/security/user/ShopUserDetails.java
+++ src/main/java/com/alxkls/eshop_backend/security/user/ShopUserDetails.java
@@ -1,4 +1,66 @@
 package com.alxkls.eshop_backend.security.user;
 
-public class ShopUserDetails {
+import com.alxkls.eshop_backend.model.User;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ShopUserDetails implements UserDetails {
+
+  private long id;
+  private String email;
+  private String password;
+
+  private Collection<GrantedAuthority> authorities;
+
+  public static ShopUserDetails createShopUserDetails(User user) {
+    List<GrantedAuthority> authorities = user.getRoles().stream().map( role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toUnmodifiableList());
+
+    return new ShopUserDetails(user.getId(), user.getEmail(), user.getPassword(),authorities);
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return authorities;
+  }
+
+  @Override
+  public String getPassword() {
+    return password;
+  }
+
+  @Override
+  public String getUsername() {
+    return email;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return UserDetails.super.isAccountNonExpired();
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return UserDetails.super.isAccountNonLocked();
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return UserDetails.super.isCredentialsNonExpired();
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return UserDetails.super.isEnabled();
+  }
 }
--- src/main/java/com/alxkls/eshop_backend/security/user/ShopUserDetailsService.java
+++ src/main/java/com/alxkls/eshop_backend/security/user/ShopUserDetailsService.java
@@ -1,4 +1,22 @@
 package com.alxkls.eshop_backend.security.user;
 
-public class ShopUserDetailsService {
+import com.alxkls.eshop_backend.model.User;
+import com.alxkls.eshop_backend.repository.user.UserRepository;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class ShopUserDetailsService implements UserDetailsService {
+  private final UserRepository userRepository;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    User user =
+        Optional.ofNullable(userRepository.findByEmail(username))
+            .orElseThrow(() -> new UsernameNotFoundException("'" + username + "' not found!"));
+    return ShopUserDetails.createShopUserDetails(user);
+  }
 }
